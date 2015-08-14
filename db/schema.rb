1c7e5ab033d0723a64e51f857359bcac8e9637a8
ruaghain <ruaghain_dev@hotmail.com>
2015-08-14 22:41:26 +0100

    Finished off the blog application

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,16 @@
 ActiveRecord::Schema.define(version: 20150814153155) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
+    t.string   "title"
+    t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id"
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
