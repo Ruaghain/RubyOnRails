b40c8350f080ea54b72669fda50009d26ad3aa9e
ruaghain <ruaghain_dev@hotmail.com>
2015-08-16 20:16:24 +0100

    Moved all the files to a new blog directory

--- Blog/config/routes.rb
+++ Blog/config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  resources :articles do
+  concern :commentable do
     resources :comments
   end
+  # You can have the root of your site routed with "root"
+  resources :articles, concerns: :commentable
+
 
   root 'welcome#index'
 
